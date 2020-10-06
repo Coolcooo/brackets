2e4a440835e0f6b8641c726622d84316bca643f4
Vladimir Privezenov <privezenoff@yandex.ru>
2020-10-07 01:35:44 +0300

    feat:add resolve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  for (let i of bracketsConfig) {
+      brackets[i[0]] = i[1];
+  }
+    let c = [];
+  for(const i of str) {
+    if (i === brackets[c[0]]) {
+        c.shift()
+    } else if (Object.keys(brackets).includes(i)) {
+        c.unshift(i)
+
+    } else {
+        return false;
+    }
+  }
+    return c.length === 0;
+
 }
+
+
